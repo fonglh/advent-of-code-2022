d509890b147abddc40f2cb4966417b2ddb49453d
fonglh <fonglh@users.noreply.github.com>
2022-12-02 17:48:14 +0800

    Day 01. Calorie Counting.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,40 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxCalories = 0
+        var elfCalories = 0
+
+        input.forEach {
+            if(it.isNotBlank()) {
+                elfCalories += it.toInt()
+            } else {
+                if (elfCalories > maxCalories) {
+                    maxCalories = elfCalories
+                }
+                elfCalories = 0
+            }
+        }
+
+        return maxCalories
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var elfCalories = 0
+        var allElfCalories = mutableListOf<Int>()
+
+        input.forEach {
+            if(it.isNotBlank()) {
+                elfCalories += it.toInt()
+            } else {
+                allElfCalories.add(elfCalories)
+                elfCalories = 0
+            }
+        }
+        return allElfCalories.sortedDescending().slice(0..2).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
