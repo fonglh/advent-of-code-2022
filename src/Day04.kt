3356828bd20eb542c863cb4c7f23336942b2f2d2
fonglh <fonglh@users.noreply.github.com>
2022-12-05 17:24:40 +0800

    Day 04. Camp Cleanup.
    Range overlap checking.

---
+++ src/Day04.kt
@@ -0,0 +1,56 @@
+fun main() {
+    fun fullyContains(firstElf: List<Int>, secondElf: List<Int>): Boolean {
+        val min1 = firstElf[0]
+        val max1 = firstElf[1]
+        val min2 = secondElf[0]
+        val max2 = secondElf[1]
+
+        return (min1 <= min2 && max1 >= max2) ||
+                (min2 <= min1 && max2 >= max1)
+    }
+
+    fun overlaps(firstElf: List<Int>, secondElf: List<Int>): Boolean {
+        val min1 = firstElf[0]
+        val max1 = firstElf[1]
+        val min2 = secondElf[0]
+        val max2 = secondElf[1]
+
+        // https://stackoverflow.com/a/3269471
+        return min1 <= max2 && min2 <= max1
+    }
+
+    fun part1(input: List<String>): Int {
+        var count = 0
+        input.forEach {
+            val elfAssignments = it.split(",")
+            val firstElf = elfAssignments[0].split("-").map { element -> element.toInt() }
+            val secondElf = elfAssignments[1].split("-").map { element -> element.toInt() }
+            if (fullyContains(firstElf, secondElf)) {
+                count += 1
+            }
+        }
+        return count
+    }
+
+    fun part2(input: List<String>): Int {
+        var count = 0
+        input.forEach {
+            val elfAssignments = it.split(",")
+            val firstElf = elfAssignments[0].split("-").map { element -> element.toInt() }
+            val secondElf = elfAssignments[1].split("-").map { element -> element.toInt() }
+            if (overlaps(firstElf, secondElf)) {
+                count += 1
+            }
+        }
+        return count
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day04_test")
+ //   check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
+
+    val input = readInput("Day04")
+//    println(part1(input))
+    println(part2(input))
+}
