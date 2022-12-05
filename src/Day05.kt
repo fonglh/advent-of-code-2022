f3ccf74f4648683287a4ce1723bae9fd39d288f3
fonglh <fonglh@users.noreply.github.com>
2022-12-05 22:06:09 +0800

    Day 05. Supply Stacks Part 2.
    Moved init code into functions to reduce duplication.
    movePartTwo uses an intermediate stack to preserve the order.

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,31 +1,8 @@
 fun main() {
-    fun move(supplyStacks: List<ArrayDeque<Char>>, numToMove: Int, src: Int, dst: Int) {
-        for(i in 1..numToMove) {
-            supplyStacks[dst-1].addLast(supplyStacks[src-1].removeLast())
-        }
-    }
+    val inputLineRegex = """move (\d+) from (\d+) to (\d+)""".toRegex()
 
-    fun topCrates(supplyStacks: List<ArrayDeque<Char>>): String {
-        val result = StringBuilder()
-        for (stack in supplyStacks) {
-            result.append(stack.lastOrNull())
-
-        }
-        return result.toString()
-    }
-
-    fun part1(input: List<String>): String {
-        val inputLineRegex = """move (\d+) from (\d+) to (\d+)""".toRegex()
-
-        // Test start state
-//        var supplyStacks = List<ArrayDeque<Char>>(3) {
-//            ArrayDeque()
-//        }
-//        supplyStacks[0].addAll(listOf('Z', 'N'))
-//        supplyStacks[1].addAll(listOf('M', 'C', 'D'))
-//        supplyStacks[2].addAll(listOf('P'))
-
-        // Full input start state
+    // Full input start state
+    fun initSupplyStacks(): List<ArrayDeque<Char>> {
         var supplyStacks = List<ArrayDeque<Char>>(9) {
             ArrayDeque()
         }
@@ -39,6 +16,49 @@ fun main() {
         supplyStacks[7].addAll(listOf('B', 'D', 'Z'))
         supplyStacks[8].addAll(listOf('M', 'N', 'Z', 'W'))
 
+        return supplyStacks
+    }
+
+    // Test start state
+    fun initTestSupplyStacks(): List<ArrayDeque<Char>> {
+        var supplyStacks = List<ArrayDeque<Char>>(3) {
+            ArrayDeque()
+        }
+        supplyStacks[0].addAll(listOf('Z', 'N'))
+        supplyStacks[1].addAll(listOf('M', 'C', 'D'))
+        supplyStacks[2].addAll(listOf('P'))
+
+        return supplyStacks
+    }
+
+    fun move(supplyStacks: List<ArrayDeque<Char>>, numToMove: Int, src: Int, dst: Int) {
+        for(i in 1..numToMove) {
+            supplyStacks[dst-1].addLast(supplyStacks[src-1].removeLast())
+        }
+    }
+
+    fun movePartTwo(supplyStacks: List<ArrayDeque<Char>>, numToMove: Int, src: Int, dst: Int) {
+        var tmpStack = ArrayDeque<Char>(numToMove)
+
+        for(i in 1..numToMove) {
+            tmpStack.addLast(supplyStacks[src-1].removeLast())
+        }
+
+        for(i in 1..numToMove) {
+            supplyStacks[dst-1].addLast(tmpStack.removeLast())
+        }
+    }
+
+    fun topCrates(supplyStacks: List<ArrayDeque<Char>>): String {
+        val result = StringBuilder()
+        for (stack in supplyStacks) {
+            result.append(stack.lastOrNull())
+
+        }
+        return result.toString()
+    }
+
+    fun part1(supplyStacks: List<ArrayDeque<Char>>, input: List<String>): String {
         input.forEach {
             val (numToMove, src, dst) = inputLineRegex.matchEntire(it)
                 ?.destructured
@@ -49,16 +69,23 @@ fun main() {
         return topCrates(supplyStacks)
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(supplyStacks: List<ArrayDeque<Char>>, input: List<String>): String {
+        input.forEach {
+            val (numToMove, src, dst) = inputLineRegex.matchEntire(it)
+                ?.destructured
+                ?: throw IllegalArgumentException("Incorrect input line $it")
+            movePartTwo(supplyStacks, numToMove.toInt(), src.toInt(), dst.toInt())
+        }
+
+        return topCrates(supplyStacks)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
-    //check(part1(testInput) == "CMZ")
-    //check(part2(testInput) == 4)
+    check(part1(initTestSupplyStacks(), testInput) == "CMZ")
+    check(part2(initTestSupplyStacks(), testInput) == "MCD")
 
     val input = readInput("Day05")
-    println(part1(input))
-    println(part2(input))
+    println(part1(initSupplyStacks(), input))
+    println(part2(initSupplyStacks(), input))
 }
