15de100e14f424e8137dc5b31d0bac213abd1151
fonglh <fonglh@users.noreply.github.com>
2022-12-14 17:17:30 +0800

    Day 12. Hill climbing algorithm.
    Dijkstra with priority queue is not needed.
    BFS is sufficient.
    Should only add new nodes with no existing distance to the queue or it
    will grow nonstop and the program never finishes.

---
+++ src/Day12.kt
@@ -0,0 +1,97 @@
+import kotlin.math.absoluteValue
+
+fun main() {
+    fun buildMap(input: List<String>): Array<CharArray> {
+        var map = Array(input.size) { CharArray(input[0].length) }
+        input.forEachIndexed { index, line ->
+           map[index] = line.toCharArray()
+        }
+        return map
+    }
+
+    fun findStartAndEnd(map: Array<CharArray>): Pair<Pair<Int, Int>, Pair<Int, Int>> {
+        var start: Pair<Int, Int> = Pair(0, 0)
+        var end: Pair<Int, Int> = Pair(0, 0)
+
+        map.forEachIndexed { rowNumber, row ->
+            row.forEachIndexed { colNumber, col ->
+                if (col == 'S') {
+                    start = Pair(rowNumber, colNumber)
+                    map[rowNumber][colNumber] = 'a'
+                } else if (col == 'E') {
+                    end = Pair(rowNumber, colNumber)
+                    map[rowNumber][colNumber] = 'z'
+                }
+            }
+        }
+
+        return Pair(start, end)
+    }
+
+    fun isValidMove(map: Array<CharArray>, curr: Pair<Int, Int>, nextStep: Pair<Int, Int>): Boolean {
+        val currHeight = map[curr.first][curr.second].hashCode()
+        val nextHeight = map[nextStep.first][nextStep.second].hashCode()
+
+        return nextHeight in 0..currHeight+1
+    }
+
+    fun part1(input: List<String>): Int {
+        var map = buildMap(input)
+        val distances = HashMap<Pair<Int, Int>, Int>(map.size * map[0].size)
+        val visited = mutableSetOf<Pair<Int, Int>>()
+        val queue = mutableListOf<Pair<Int, Int>>()
+        val startAndEnd = findStartAndEnd(map)
+        val start = startAndEnd.first
+        val end = startAndEnd.second
+
+        distances[start] = 0
+        queue.add(start)
+        visited.add(start)
+        while(queue.isNotEmpty()) {
+            val curr = queue.removeFirst()
+            val currDistance = distances[curr]!!
+            // reached the top
+            if (curr == end) {
+                return distances[curr]!!
+            }
+
+            for (i in -1..1) {
+                for (j in -1..1) {
+                    //offsets will give up, down, left right
+                    if (i.absoluteValue != j.absoluteValue) {
+                        // check if new coords are valid
+                        val nextStep = Pair(curr.first + i, curr.second + j)
+                        if (nextStep.first >=0 && nextStep.first < map.size
+                            && nextStep.second >= 0 && nextStep.second < map[0].size
+                            && !visited.contains(nextStep)
+                            && isValidMove(map, curr, nextStep)) {
+                            // no need to handle relaxation step where distances get reduced
+                            // since all distances are equal
+                            if (!distances.containsKey(nextStep)) {
+                                distances[nextStep] = 1 + currDistance
+                                queue.add(nextStep)
+                            }
+                        }
+                    }
+                }
+            }
+            visited.add(curr)
+        }
+
+        return 0
+    }
+
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day12_test")
+    //check(part1(testInput) == 31)
+    check(part2(testInput) == 0)
+
+    val input = readInput("Day12")
+    println(part1(input))
+    println(part2(input))
+}
