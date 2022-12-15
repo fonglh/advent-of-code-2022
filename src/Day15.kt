163724a72b5fe48c6422dcedf2a0aa829fae4e84
fonglh <fonglh@users.noreply.github.com>
2022-12-16 01:27:45 +0800

    Day 15. Beacon Exclusion Zone Part 2.
    Brute force approach works for the test input but the
    search space is way too large for the actual input.
    Find the perimeter of each sensor and check those points instead.

--- src/Day15.kt
+++ src/Day15.kt
@@ -36,6 +36,56 @@ fun main() {
         return beacons.size
     }
 
+    fun getPerimeterPoints(sensor: SensorData, limit: Int): Set<Pair<Int, Int>> {
+        var perimeterPoints = mutableSetOf<Pair<Int, Int>>()
+
+        // top to right
+        for (deltaX in 0..(sensor.distance+1)) {
+            val deltaY = -(sensor.distance+1 - deltaX)
+            val perimeterX = sensor.sensorPos.first + deltaX
+            val perimeterY = sensor.sensorPos.second + deltaY
+
+            if (perimeterX in 0..limit && perimeterY in 0..limit) {
+                perimeterPoints.add(Pair(perimeterX, perimeterY))
+            }
+        }
+
+        // right to bottom
+        for (deltaX in (sensor.distance+1) downTo 0) {
+            val deltaY = sensor.distance+1 - deltaX
+            val perimeterX = sensor.sensorPos.first + deltaX
+            val perimeterY = sensor.sensorPos.second + deltaY
+
+            if (perimeterX in 0..limit && perimeterY in 0..limit) {
+                perimeterPoints.add(Pair(perimeterX, perimeterY))
+            }
+        }
+
+        // bottom to left
+        for (deltaX in 0 downTo -(sensor.distance+1)) {
+            val deltaY = sensor.distance+1 + deltaX
+            val perimeterX = sensor.sensorPos.first + deltaX
+            val perimeterY = sensor.sensorPos.second + deltaY
+
+            if (perimeterX in 0..limit && perimeterY in 0..limit) {
+                perimeterPoints.add(Pair(perimeterX, perimeterY))
+            }
+        }
+
+        //left to top
+        for (deltaX in -(sensor.distance+1)..0) {
+            val deltaY = -(sensor.distance+1 + deltaX)
+            val perimeterX = sensor.sensorPos.first + deltaX
+            val perimeterY = sensor.sensorPos.second + deltaY
+
+            if (perimeterX in 0..limit && perimeterY in 0..limit) {
+                perimeterPoints.add(Pair(perimeterX, perimeterY))
+            }
+        }
+
+        return perimeterPoints
+    }
+
     fun part1(input: List<String>, targetRow: Int): Int {
         var allSensorData = mutableListOf<SensorData>()
 
@@ -62,16 +112,37 @@ fun main() {
         return count - beaconsInRow
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<String>, maxSearchCoord: Int): Long {
+        var allSensorData = mutableListOf<SensorData>()
+
+        input.forEach {
+            val (sensorX, sensorY, beaconX, beaconY) = inputLineRegex.matchEntire(it)
+                ?.destructured
+                ?: throw IllegalArgumentException("Incorrect input line $it")
+            val sensorPos = Pair(sensorX.toInt(), sensorY.toInt())
+            val beaconPos = Pair(beaconX.toInt(), beaconY.toInt())
+            val distance = manhattanDistance(sensorPos, beaconPos)
+            allSensorData.add(SensorData(sensorPos, beaconPos, distance))
+        }
+
+        allSensorData.forEach {sensor: SensorData ->
+            val searchPoints = getPerimeterPoints(sensor, maxSearchCoord)
+            searchPoints.forEach { candidatePos: Pair<Int, Int> ->
+                if (allSensorData.none { manhattanDistance(it.sensorPos, candidatePos) <= it.distance }) {
+                    println(candidatePos)
+                    return (candidatePos.first.toLong() * 4000000.toLong() + candidatePos.second.toLong())
+                }
+            }
+        }
         return 0
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day15_test")
     check(part1(testInput, 10) == 26)
-    check(part2(testInput) == 0)
+    check(part2(testInput, 20) == 56000011.toLong())
 
     val input = readInput("Day15")
     println(part1(input, 2000000))
-    println(part2(input))
+    println(part2(input, 4000000))
 }
