bad487a4164fb806fe5e3939f591eb84d4d0cc60
fonglh <fonglh@users.noreply.github.com>
2022-12-15 10:43:53 +0800

    Day 12. Hill climbing algorithm Part 2.
    Instead of trying all the 'a' locations and climbing up, start from the
    end point and climb down to the first 'a'.

--- src/Day12.kt
+++ src/Day12.kt
@@ -81,15 +81,66 @@ fun main() {
         return 0
     }
 
+    // climb down by one, climb up by any amount.
+    fun isValidMovePart2(map: Array<CharArray>, curr: Pair<Int, Int>, nextStep: Pair<Int, Int>): Boolean {
+        val currHeight = map[curr.first][curr.second].hashCode()
+        val nextHeight = map[nextStep.first][nextStep.second].hashCode()
+
+        return nextHeight >= (currHeight - 1)
+    }
 
     fun part2(input: List<String>): Int {
+        var map = buildMap(input)
+        val distances = HashMap<Pair<Int, Int>, Int>(map.size * map[0].size)
+        val visited = mutableSetOf<Pair<Int, Int>>()
+        val queue = mutableListOf<Pair<Int, Int>>()
+        val startAndEnd = findStartAndEnd(map)
+        val start = startAndEnd.first
+        val end = startAndEnd.second
+
+        // start from the E point and look for the first 'a'
+        distances[end] = 0
+        queue.add(end)
+        visited.add(end)
+        while(queue.isNotEmpty()) {
+            val curr = queue.removeFirst()
+            val currDistance = distances[curr]!!
+            // reached the bottom
+            if (map[curr.first][curr.second] == 'a') {
+                return distances[curr]!!
+            }
+
+            for (i in -1..1) {
+                for (j in -1..1) {
+                    //offsets will give up, down, left right
+                    if (i.absoluteValue != j.absoluteValue) {
+                        // check if new coords are valid
+                        val nextStep = Pair(curr.first + i, curr.second + j)
+                        if (nextStep.first >=0 && nextStep.first < map.size
+                            && nextStep.second >= 0 && nextStep.second < map[0].size
+                            && !visited.contains(nextStep)
+                            && isValidMovePart2(map, curr, nextStep)) {
+                            // no need to handle relaxation step where distances get reduced
+                            // since all distances are equal
+                            if (!distances.containsKey(nextStep)) {
+                                distances[nextStep] = 1 + currDistance
+                                queue.add(nextStep)
+                            }
+                        }
+                    }
+                }
+            }
+            visited.add(curr)
+        }
+
+        println("why here?")
         return 0
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day12_test")
-    //check(part1(testInput) == 31)
-    check(part2(testInput) == 0)
+    check(part1(testInput) == 31)
+    check(part2(testInput) == 29)
 
     val input = readInput("Day12")
     println(part1(input))
