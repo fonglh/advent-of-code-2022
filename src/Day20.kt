8163f4a3f327ad272b8a7dad1f95ec5372cdbbdf
fonglh <fonglh@users.noreply.github.com>
2022-12-20 18:06:16 +0800

    Day 20 Part 2. Grove Positioning System.
    Replace loops with math calculations for the new index value.
    Mod can handle positive indices.
    The -= for negative indices is repeated substraction, which is similar
    to division.
    
    Tested on part1 then implemented for part2. Use Long instead of Int to
    prevent silent overflow indexing errors.

--- src/Day20.kt
+++ src/Day20.kt
@@ -1,11 +1,11 @@
 fun main() {
-    data class InputNumber(val originalIndex: Int, val value: Int)
+    data class InputNumber(val originalIndex: Int, val value: Long)
 
-    fun numberAt(input: List<InputNumber>, positionFromZero: Int): Int {
+    fun numberAt(input: List<InputNumber>, positionFromZero: Int): Long {
         // find position of 0
         var zeroIndex = 0
         for (i in input.indices) {
-            if (input[i].value == 0) {
+            if (input[i].value == 0.toLong()) {
                 zeroIndex = i
                 break
             }
@@ -14,35 +14,61 @@ fun main() {
     }
 
     fun part1(input: List<String>): Int {
-        val inputNumbers = input.mapIndexed { idx, value -> InputNumber(idx, value.toInt()) }
+        val inputNumbers = input.mapIndexed { idx, value -> InputNumber(idx, value.toLong()) }
         val inputSize = inputNumbers.size
         var outputNumbers = inputNumbers.toMutableList()
+
         inputNumbers.forEach {
             val currIndex = outputNumbers.indexOf(it)
             var newIndex = (currIndex + it.value)
-            while (newIndex <= 0) {
-                newIndex += (inputSize-1)
+            if (newIndex <= 0) {
+                // quotient is negative
+                val quotient = newIndex / (inputSize-1)
+                newIndex += (-quotient+1) * (inputSize-1)
             }
-            while (newIndex > inputSize) {
-                newIndex -= (inputSize-1)
+            if (newIndex >= inputSize) {
+                newIndex %= (inputSize-1)
             }
             outputNumbers.removeAt(currIndex)
-            outputNumbers.add(newIndex, it)
+            outputNumbers.add(newIndex.toInt(), it)
         }
 
-        return numberAt(outputNumbers, 1000) +
+        return (numberAt(outputNumbers, 1000) +
                 numberAt(outputNumbers, 2000) +
-                numberAt(outputNumbers, 3000)
+                numberAt(outputNumbers, 3000)).toInt()
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        val inputNumbers = input.mapIndexed { idx, value -> InputNumber(idx, value.toLong() * 811589153.toLong()) }
+        val inputSize = inputNumbers.size
+        var outputNumbers = inputNumbers.toMutableList()
+
+        repeat(10) {
+            inputNumbers.forEach {
+                val currIndex = outputNumbers.indexOf(it)
+                var newIndex = (currIndex.toLong() + it.value)
+                if (newIndex <= 0) {
+                    // quotient is negative
+                    val quotient = newIndex / (inputSize-1).toLong()
+                    newIndex += (-quotient+1.toLong()) * (inputSize-1).toLong()
+                }
+                if (newIndex >= inputSize) {
+                    newIndex %= (inputSize-1).toLong()
+                }
+                outputNumbers.removeAt(currIndex)
+                outputNumbers.add(newIndex.toInt(), it)
+            }
+        }
+
+        return (numberAt(outputNumbers, 1000) +
+                numberAt(outputNumbers, 2000) +
+                numberAt(outputNumbers, 3000))
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day20_test")
     check(part1(testInput) == 3)
-    check(part2(testInput) == 0)
+    check(part2(testInput) == 1623178306.toLong())
 
     val input = readInput("Day20")
     println(part1(input))
